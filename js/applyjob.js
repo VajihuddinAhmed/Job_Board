bd5882f5ffdb0a7d92b40c0bf8cc8a21d24013fb
VajihuddinAhmed <vajihahmed@gmail.com>
2020-10-24 02:25:02 +0200

    Apply job page completed

--- js/applyjob.js
+++ js/applyjob.js
@@ -23,7 +23,7 @@ fetch(url)
         jobType.setAttribute("class", "jobs")
 
         const applyJob = document.createElement('button')
-        applyJob.setAttribute("id", "applyJob")
+        applyJob.setAttribute("id", "applyJob" + i)
 
         const row = document.createElement('hr')
         row.setAttribute("id", "row")
@@ -49,11 +49,47 @@ fetch(url)
         buttonDiv.appendChild(applyJob)
         jobList.appendChild(buttonDiv)
         jobList.appendChild(row)
-    })
 
+        const modal = document.querySelector(".modal");
+        let appJob =  document.querySelector("#applyJob" + i);
+        const apply = document.querySelector("#apply");
+        const closeButton = document.querySelector(".close-button");
+        function toggleModal() {
+            modal.classList.toggle("show-modal");
+        }
+
+        function windowOnClick(event) {
+            if (event.target === modal) {
+                toggleModal();
+            }
+        }
+        
+        appJob.addEventListener("click", () => {
+            if(apply) {
+                apply.innerHTML = ''
+            }
+            const spanName = document.createElement('p');
+            spanName.textContent = `You are applying to "${item.JobTitle}" at "${item.Company}"`
+            apply.appendChild(spanName);
+            modal.classList.toggle("show-modal");
+            const submit = document.querySelector('#start')
+            submit.addEventListener('click', () => {
+                const emailValue = document.querySelector('#mail').value
+                localStorage.setItem('User Email', emailValue)
+                document.querySelector('#mail').value = ''
+                const uploadFile = document.querySelector('#file').value
+                localStorage.setItem('User Resume', uploadFile)
+                document.querySelector('#file').value = '' 
+                modal.classList.toggle("show-modal");
+                const successJob = document.querySelector('#success-job')
+                successJob.textContent = `Applied to "${item.JobTitle}" at "${item.Company}" successfully...`
+            })
+            
+        });
+        closeButton.addEventListener("click", toggleModal);
+        window.addEventListener("click", windowOnClick);
+    })
 })
 .catch((error) => {
     console.log(error)
 })
-
-
